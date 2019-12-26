275467920492a90c6fe0086e8a0f3718ddec7f4f
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2019-12-26 17:10:53 +1300

    First test

---
+++ tests/unit/BaseIndexExtensionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Firesphere\SolrPermissions\Tests;
+
+
+use Firesphere\SolrPermissions\Extensions\BaseIndexExtension;
+use Firesphere\SolrSearch\Queries\BaseQuery;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\DataObject;
+use Solarium\QueryType\Select\Query\Query;
+
+class BaseIndexExtensionTest extends SapphireTest
+{
+
+    public function testOnBeforeSearch()
+    {
+        $object = new CircleCITestIndex();
+        $extension = new BaseIndexExtension();
+        $extension->setOwner($object);
+
+        $query = new BaseQuery();
+        $clientQuery = new Query();
+
+        $result = $extension->onBeforeSearch($query, $clientQuery);
+
+        $this->assertEquals(['MemberView' => ['null']], $query->getFilter());
+    }
+}
