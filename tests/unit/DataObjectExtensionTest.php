41f662e0e0a3c51453f838f533c5036a1008fc77
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2019-12-29 10:51:24 +1300

    Add DO Extension test

---
+++ tests/unit/DataObjectExtensionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Firesphere\SolrPermissions\Tests;
+
+
+use Firesphere\SolrPermissions\Extensions\DataObjectExtension;
+use SilverStripe\Dev\SapphireTest;
+
+class DataObjectExtensionTest extends SapphireTest
+{
+
+    public function testGetMemberView()
+    {
+        $page = \Page::get()->first();
+        $extension = new DataObjectExtension();
+        $extension->setOwner($page);
+
+        $viewStatus = $extension->getMemberView();
+
+        $this->assertEquals(['null'], $viewStatus);
+
+        $page->CanViewType('LoggedInUsers');
+        $page->write();
+
+        $extension->setOwner($page);
+
+        $status = $extension->getMemberView();
+
+        $this->assertNotContains('null', $status);
+    }
+}
